f2d02a2c4d276431df07b157ab375f0972eb7d2e
Ephie111 <enehsteph31@gmail.com>
2026-09-10 20:55:51 +0100

    update the role in team member

--- components/TeamSection.tsx
+++ components/TeamSection.tsx
@@ -17,7 +17,7 @@ const OPERATIONS_TEAM: TeamMember[] = [
   { name: "Naanlang Haruna Amos", role: "Operations Team", photo: "/images/team/amosharuna.jpg" },
   { name: "Ezra Melchizedek", role: "Operations Team", photo: "/images/team/ezramelchizedek.jpg" },
   { name: "Nnaji Ogechi Elizabeth", role: "Operations Team", photo: "/images/team/nnajiogechi.jpg" },
-  { name: "Ezeh Rosemary Chidimma", role: "Oeration Manager",  photo: "/images/team/ezehrosemary.jpg"},
+  { name: "Ezeh Rosemary Chidimma", role: "Operation Manager",  photo: "/images/team/ezehrosemary.jpg"},
   { name: "Chinyere Foreign Okoli", role: "Business Coordinator", photo: "/images/team/chinyereokoli.jpg" },
   { name: "Engr. Onyeka Eze", role: "Site Engineer", photo: "/images/team/onyekaeze.jpg"},
   { name: "Arc. Peter Okpala", role: "Head of Constructions",  photo: "/images/team/uchennaokpala.jpg" },
